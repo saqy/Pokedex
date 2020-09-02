f4446f23e60869318c8ef1a30ac4f7857cf58d58
Muhammad Saqib <muhammadsaqiib@gmail.com>
2020-09-02 10:56:29 -0400

    fixes base url

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="pokedex" >
+    <BrowserRouter basename="Pokedex" >
       <App />
     </BrowserRouter>
   </React.StrictMode>,
