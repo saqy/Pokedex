c13ea5bde155683bf283d1dfd97dbdd720733112
Muhammad Saqib <muhammadsaqiib@gmail.com>
2020-09-02 10:37:23 -0400

    Updates

---
+++ precache-manifest.982ad4d2e281c386410d7b7e3e4a9c18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c139ed00e272ca1d6b395ea2851bdc32",
+    "url": "https://saqy.github.io/pokedex/index.html"
+  },
+  {
+    "revision": "4df55df1b2f09edfc668",
+    "url": "https://saqy.github.io/pokedex/static/js/2.47e28627.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://saqy.github.io/pokedex/static/js/2.47e28627.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "784104d0c2c01de291c1",
+    "url": "https://saqy.github.io/pokedex/static/js/main.4743ef4c.chunk.js"
+  },
+  {
+    "revision": "e1ff2850718b68a2fd9a",
+    "url": "https://saqy.github.io/pokedex/static/js/runtime-main.142ba853.js"
+  }
+]);
